b9a5cb94f7297eb2b87c6875d8b95473ac58a609
muzammil206 <ismailmuzammil206@gmail.com>
2025-10-20 10:42:36 +0100

    added login page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import AdminDashboard from '@/components/admin'
 import {AdminLayout} from "@/components/layout-admin"
+import AdminLoginPage from "@/components/admin-login-page"
 
 export default function page() {
   return (
-    <AdminLayout>
-      <AdminDashboard />
-    </AdminLayout>
+    <AdminLoginPage />
   )
 }
